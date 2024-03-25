861e74224e882493dbef9a7b3d37101df19edf66
hridayroy19 <hrhridoyroy503@gmail.com>
2024-03-25 23:35:39 +0600

    update add modal box

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,40 +1,108 @@
 import React from 'react';
 
 const Add = () => {
+    const handelAdd = e =>{
+        e.preventDefault()
+        const from = e.target
+        const title = from.title.value
+        const description = from.description.value
+        const deadline = from.deadline.value
+        const assign = from.assign.value
+        const priority = from.priority.value
+        const  status = from. status.value
+        console.log(title,description,deadline,assign,priority, status);
+    }
     return (
         <div>
             <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
-            <div className="modal-box">
-                <div className="flex justify-between px-3 ">
-                    <p></p>
-                    <form method="dialog">
-                        <button className="btn btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                <div className="modal-box bg-gray-200">
+                    <div className="flex justify-between px-3 ">
+                        <p></p>
+                        <form method="dialog">
+                            <button className="btn btn-circle">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                            </button>
+                        </form>
+                    </div>
+                    <form onSubmit={handelAdd}>
+                        <div className="flex flex-col mb-5">
+                            <label className="mb-2">
+                                Title
+                            </label>
+                            <input
+                                className="w-full px-2 py-2 rounded-md"
+                                type="text"
+                                name="title"
+                            />
+                        </div>
+                        <div className="flex flex-col mb-5">
+                            <label className="mb-2">
+                                Description
+                            </label>
+                            <textarea
+                                className="w-full px-2 py-2 rounded-md"
+                                type="text"
+                                name="description"
+                            />
+                        </div>
+                        <div className="flex flex-col mb-5">
+                            <label htmlFor="title" className="mb-2">
+                                Deadline
+                            </label>
+                            <input
+                                className="w-full px-2 py-1 rounded-md"
+                                type="date"
+                                name="deadline"
+                            />
+                        </div>
+                        <div className="flex flex-col mb-5">
+                            <label htmlFor="title" className="mb-2">
+                                Assign
+                            </label>
+                            <input
+                                className="w-full px-2 py-2 rounded-md"
+                                type="text"
+                                name="assign"
+                            />
+                        </div>
+                        <div className='flex justify-between mt-3 mb-2'>
+                            <div className="flex flex-col mb-5">
+                                <label htmlFor="title" className="mb-2">
+                                    Priority
+                                </label>
+                                <select
+                                    className="w-full px-1 rounded-md"
+                                    name="priority"
+                                >
+                                    <option defaultValue value="P0">
+                                        P0
+                                    </option>
+                                    <option value="P1">P1</option>
+                                    <option value="P2" > P2</option>
+                                </select>
+                            </div>
+                            <div className="flex flex-col mb-5">
+                                <label htmlFor="title" className="mb-2">
+                                    Status
+                                </label>
+                                <select
+                                    className="w-full px-1 rounded-md"
+                                    name="status"
+                                >
+                                    <option value="Assign"> Assign </option>
+                                    <option value="InProgress"> In Progress </option>
+                                    <option value="Completed"> Completed </option>
+                                    <option value="Deployed"> Deployed </option>
+                                    <option value="Deferred"> Deferred </option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary ">
+                            Submit
                         </button>
                     </form>
                 </div>
-
-                <form className="card-body " method="dialog">
-                    <h3 className="font-bold text-2xl">Please Login </h3>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control mt-6">
-                        <button type="submit" className="btn bg-green text-white">Login</button>
-                    </div>
-                </form>                                 
-            </div>
-          
-        </dialog>
+            </dialog>
         </div>
     );
 };
